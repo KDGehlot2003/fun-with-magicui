e07fbb6566433e5b7053b26ee200ae6695d9a083
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-07-05 20:44:03 +0530

    feat: Add FlipTextDemo component for flipping text animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,28 @@ import {MarqueeDemo} from "./components/MovingCards"
 import Globe from "./components/magicui/globe"
 import {DockDemo} from "./components/Dock"
 import { IconCloudDemo } from "./components/Cloud"
+import FlipTextDemo from "./components/Text"
+import {GridPatternDemo} from "./components/Grid"
 
 
 function App() {
 
   return (
-    <div>
+    <div >
       {/* <div className="">
         <Globe />
       </div>
       <div className=" fixed">
         <MarqueeDemo />
       </div> */}
-      <div className="grid grid-cols-3 gap-5">
+      <div className="flex justify-center">
+        <GridPatternDemo />
+      </div>
+      <div className="grid grid-cols-3 grid-rows-2 gap-5">
         <div className=" col-span-2">
-          <h1 className="text-6xl font-bold mt-52 text-center">Kshitij Gehlot</h1>
+          <h1 className="text-6xl font-bold mt-60 text-center"><FlipTextDemo /></h1>
         </div>
-        <div className="">
+        <div className="row-span-2">
           <IconCloudDemo />
         </div>
       </div>
---
+++ src/components/Textgs.tsx
@@ -0,0 +1,12 @@
+import GradualSpacing from "../components/magicui/gradual-spacing";
+
+const Textgs = () => {
+  return (
+    <GradualSpacing
+      className="font-display text-center text-4xl font-bold tracking-[-0.1em]  text-black dark:text-white md:text-7xl md:leading-[5rem]"
+      text="Kshitij Gehlot"
+    />
+  )
+}
+
+export default Textgs
